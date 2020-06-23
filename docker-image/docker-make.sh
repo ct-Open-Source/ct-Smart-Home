5af0bf0cd735fcc9c7cbdc6fb26f32b4958b732e
Merlin Schumacher <merlin.schumacher@gmail.com>
2020-06-23 18:05:41 +0200

    fix bluetooth modules and switched to modern noble implementation

--- docker-image/docker-make.sh
+++ docker-image/docker-make.sh
@@ -5,7 +5,7 @@ echo "#########################################################################"
 echo "node-red version: ${NODE_RED_VERSION}"
 echo "#########################################################################"
 
-docker build --no-cache \
+docker build \
     --build-arg ARCH=amd64 \
     --build-arg NODE_VERSION=12 \
     --build-arg NODE_RED_VERSION=${NODE_RED_VERSION} \
