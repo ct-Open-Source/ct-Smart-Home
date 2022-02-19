ece18dbbc2e6f1bff27874e5d176bcc632f26e97
Matze <ma4a@users.noreply.github.com>
2022-02-19 11:02:25 +0100

    Update entrypoint.sh
    Update to fix problems with /run/dbus/dbus.pid

--- docker-image/entrypoint.sh
+++ docker-image/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 [[ -e /var/run/dbus.pid ]] && su-exec root rm -f /var/run/dbus.pid
+[[ -e /run/dbus/dbus.pid ]] && su-exec root rm -f /run/dbus/dbus.pid
 [[ -e /var/run/avahi-daemon/pid ]] && su-exec root rm -f /var/run/avahi-daemon/pid
 [[ -e /var/run/dbus/system_bus_socket ]] && su-exec root rm -f /var/run/dbus/system_bus_socket
 
